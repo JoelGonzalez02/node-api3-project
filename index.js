82acdd9ce5141b3889b4eb09c62d726607de9e42
Joel Gonzalez <espjoemusic@gmail.comgir config --global user.name JoelGonzalez02>
2020-07-16 19:25:28 -0700

    added changes for heroku deployment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const server = require('./server.js');
+require('dotenv').config();
 
-const PORT = 4000;
+const port = process.env.PORT || 4000;
 
-server.listen(PORT, () =>{
-    console.log(`\n*** Server running on http://localhost: ${PORT} ***\n`);
+server.listen(port, () =>{
+    console.log(`\n*** Server running on http://localhost: ${port} ***\n`);
 });
